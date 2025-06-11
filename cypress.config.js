56bd7a3431bdd34a87b29b33dee3f6eb303fecd2
nadejdaT12 <nadejda.taigunova@gmail.com>
2025-06-11 09:54:35 +0300

    Add 2 files after installed Allure

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,29 @@
-module.exports = {
+const { defineConfig } = require('cypress')
+const {allureCypress} = require("allure-cypress/reporter")
+const {platform, release, version} = require("node:os");
+
+module.exports = defineConfig({
+  reporter: 'mochawesome',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureCypress(on, config, {
+        environmentInfo: {
+          os_platform: platform(),
+          os_release: release(),
+          os_version: version(),
+          node_version: process.version,
+        },
+        resultsDir: "allure-results",
+      })
+      return config;
     },
-    baseUrl: "https://api.clickup.com/api/v2/"
+    // Configure your E2E tests here
+    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
+    baseUrl: "https://api.clickup.com/api/v2",
+    env: {
+      allureLogCypress:false,
+      allureReuseAfterSpec: true,
+      token: 'pk_62686931_IIJURLQ4BVJWIFYESRUJR3NC9I55D56T'
+    }
   },
-};
+})
